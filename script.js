a5e1a01b4492882c7d222095331b4aa45fad5b3a
Dephanie Ho <dephho88@hotmail.com>
2017-07-25 01:13:28 -0700

    Cleaned code and renamed variables

--- script.js
+++ script.js
@@ -1,250 +1,107 @@
 //Run once DOM is ready for JavaScript to execute
-$(document).ready(function(){
-  generateGrid();
-  labelGrid();
-
-  compSetBoard();
+$(document).ready(function() {
+  startGame();
 });
 
-var numRows=10;
-var numCols=11;
+const NUMROWS = 10;
+const NUMCOLS = 11;
+const FIRSTCOL = 'A';
+const OCCUPIED = 'occupied';
+
+const NORTH = 1;
+const EAST = 2;
+const SOUTH = 3;
+const WEST = 4;
+
+//Start game
+function startGame() {
+  //Set-Up
+  generateGrid();
+  labelGrid();
+  compSetBoard();
+}
+
+//Player's turn
+function nextMove(square) {
+  /*https://stackoverflow.com/questions/41255984/how-to-get-an-id-of-the-clicked-element*/
+  if(document.getElementById(id).getAttribute('data') === OCCUPIED){
+    setMessage("Hit!");
+  }
+  else{
+    setMessage("Miss");
+  }
+}
+
 
 //Generate the battleship board grid
 function generateGrid() {
-  var markup='';
-  var letter='A';
-  var number='1';
-  
-  for(i=0; i<numRows; i++) {
+  var markup = '';
+  var column = 'A';
+  var row = '1';
+
+  for(i=0; i<NUMROWS; i++) {
     markup += '<tr>';
-    for(j=0; j<numCols; j++) {
+    for(j=0; j<NUMCOLS; j++) {
       if(j === 0){
-        if(number === ':'){
-          number = numRows.toString();  // I think this works. I didn't test
-          markup += '<td id="' + number + '"class="square"></td>';
+        if(row === ':'){
+          row = NUMROWS.toString();
+          markup += '<td id="' + row + '"class="square"></td>';
         }
         else{
-          markup += '<td id="' + j.toString() + number + '"class="square"></td>';
+          markup += '<td id="' + j.toString() + row + '"class="square"></td>';
         }
       }
       else{
-        markup += '<td id="' + letter + number + '" class="square"></td>';
-        letter=incrementChar(letter);
+        markup += '<td id="' + column + row + '" class="square"></td>';
+        column = incrementChar(column);
       }
     }
     markup += '</tr>'
-    //letter=incrementChar(letter);
-    letter='A';
-    number=incrementChar(number);
+    column = 'A';
+    row = incrementChar(row);
   }
   $('.battleship-grid').append(markup);
 }
 
 //Label the fixed portions of battleship grid
 function labelGrid() {
-  var letter='A';
-  var number='1';
-  for(i=0; i<numRows; i++){
-    insertText('0' + letter, letter);
+  var column = 'A';
+  var row = '1';
+  for(i=0; i<NUMROWS; i++){
+    insertText('0' + column, column);
 
     if(i === 9){
-      var rowLabel = numRows.toString(); // I not 100% if this is what you mean to do here. So revert if this doesn't make sense
+      var rowLabel = NUMROWS.toString();
       insertText(rowLabel, rowLabel);
     }
     else{
-      insertText('0' + number, number);
+      insertText('0' + row, row);
     }
-    letter=incrementChar(letter);
-    number=incrementChar(number);
+    column = incrementChar(column);
+    row = incrementChar(row);
   }
-}
-
-// I really like these small helper methods. It makes the actual business logic more readable
-//Increments the letter for the square ID (ex: A0, A1, B2, etc...)
-function incrementChar(letter) {
-  return String.fromCharCode(letter.charCodeAt(0) + 1);
-}
-
-//Set grid text based on square ID
-function insertText(id, letter) {
-  document.getElementById(id).innerHTML = letter;
-}
-
-//Random number generator for computer ship placements & direction
-function getRandomNumber(min, max) {
-  return Math.floor( Math.random() * ( 1 + max - min ) ) + min;
-}
-
-//Random letter generator for computer ship placements
-function getRandomLetter() {
-  return randomLetter=('ABCDEFGHIJ').split('')[(Math.floor(Math.random() * 10 ))];
-}
-
-//Random location generator for computer ship placements
-function getRandomLocation() {
-  var letter=getRandomLetter();
-  var number=getRandomNumber(1,10);
-  var pos=letter + number;
-  return pos;
-}
-
-// This guy is very confusing lol.
-// I think inheritly it will be, and I'm glad you wrote a lot of 
-// comments. 
-
-//Random direction generator for possible computer ship placements
-function getRandomDirection(letter, number) {
-  //Direction to place - 1 up, 2 right, 3 down, 4 left
-  var dir=0;
-
-  /* Checking if randomized location is on the border of the grid */
-  //If in first row, ship cannot be placed up
-  if(number===1) {
-    if(letter==='A'){
-      //If also in column A, ship cannot be placed left
-      dir=getRandomNumber(2,3);
-    }
-    else if(letter==='J') {
-      //If also in column J, ship cannot be placed right
-      dir=getRandomNumber(3,4);
-    }
-    else{
-      dir=getRandomNumber(2,4);
-    }
-  }
-  //If in column A, ship cannot be placed left
-  else if(letter==='A'){
-    //If also in last row, ship cannot be placed down
-    if(number===10){
-      dir=getRandomNumber(1,2);
-    }
-    else{
-      dir=getRandomNumber(1,3);
-    }
-  }
-  //If in column J, ship cannot be placed right
-  else if(letter==='J'){
-    //If also in last row, ship cannot be placed down
-    if(number===10){
-      dir=getRandomNumber(1,2);
-      if(dir===2){
-        // not sure what this 4 is for...I would 
-        // make it a variable so it is more readable
-        dir=4;
-      }
-    }
-    else{
-      dir=getRandomNumber(1,3);
-      if(dir===2){
-        dir=4;
-      }
-    }
-  }
-  //If location is not on border, ship can be in any direction
-  else{
-    // I think you could use the 4 variable here too?
-    dir=getRandomNumber(1,4);
-  }
-  return dir;
 }
 
 //Place the ships according to ship type(size)
-function setShip(size){
-  var pos=getRandomLocation();
-  
-  // LetterA - what is the significance of this variable? 
-  // A name that puts it into the context of the game 
-  // would be a much better name - like 
-  // first row? first column? etc...
-  var letterA='A';
-  var letter=pos[0];
-  var number=pos[1];
-  var dir=getRandomDirection(letter, number);
+function setShip(size) {
+  var pos = getRandomLocation();
+  var column = pos[0];
+  var row = pos[1];
+  var dir = getRandomDirection(column, row);
 
   //Set ship based on orientation
   switch(dir){
-      // what does case 1, 2, 3... mean? Is this veritcal? horizontal...
-      // #2: Each case could be a function on its own...You should
-      // aim for smaller functions
-    case 1:
-        if(number<size){
-          return false;
-        }
-        for(i=0; i<size; i++){
-          // should make occupied a variable and use it everywhere below
-          if(document.getElementById(pos).getAttribute('data') === 'occupied'){
-            return false;
-          }
-          pos=letter + String.fromCharCode(pos.charCodeAt(1) - 1);
-        }
-        //5 Spaces can be occupied
-        pos=letter + number;
-        for(j=0; j<size; j++){
-          document.getElementById(pos).setAttribute('data','occupied');
-          document.getElementById(pos).innerHTML='X';
-          pos=letter + String.fromCharCode(pos.charCodeAt(1) - 1);
-        }
+    case NORTH:
+        return(setShipNorth(pos, size));
         break;
-    case 2:
-        if(letter>(String.fromCharCode(letterA.charCodeAt(0) + (size-1)))){
-          return false;
-        }
-        for(i=0; i<size; i++){
-          if(document.getElementById(pos).getAttribute('data')==='occupied'){
-            return false;
-          }
-          // Looks like you use this String.fromCharCode(pos.charCodeAt(0) + 1) + number;
-          // function a few times. It would be a good helper function
-          pos=String.fromCharCode(pos.charCodeAt(0) + 1) + number;
-        }
-        //5 Spaces can be occupied
-        pos=letter + number;
-        console.log("Size: " + size);
-
-        for(j=0; j<size; j++){
-          document.getElementById(pos).setAttribute('data','occupied');
-          document.getElementById(pos).innerHTML='X';
-          pos=String.fromCharCode(pos.charCodeAt(0) + 1) + number;
-          console.log("After: " + pos);
-
-        }
+    case EAST:
+        return(setShipEast(pos, size));
         break;
-    case 3:
-        if(number>size){
-          return false;
-        }
-        for(i=0; i<size; i++){
-          if(document.getElementById(pos).getAttribute('data')==='occupied'){
-            return false;
-          }
-          pos=letter + String.fromCharCode(pos.charCodeAt(1) + 1);
-
-        }
-        //5 Spaces can be occupied
-        pos=letter + number;
-        for(j=0; j<size; j++){
-          document.getElementById(pos).setAttribute('data','occupied');
-          document.getElementById(pos).innerHTML='X';
-          pos=letter + String.fromCharCode(pos.charCodeAt(1) + 1);
-        }
+    case SOUTH:
+        return(setShipSouth(pos, size));
         break;
-    case 4:
-        if(letter<(String.fromCharCode(letterA.charCodeAt(0) + (size-1)))) {
-          return false;
-        }
-        for(i=0; i<size; i++){
-          if(document.getElementById(pos).getAttribute('data')==='occupied') {
-            return false;
-          }
-          pos=String.fromCharCode(pos.charCodeAt(0) - 1) + number;
-        }
-        //5 Spaces can be occupied
-        pos=letter + number;
-        for(j=0; j<size; j++) {
-          document.getElementById(pos).setAttribute('data','occupied');
-          document.getElementById(pos).innerHTML='X';
-          pos=String.fromCharCode(pos.charCodeAt(0) - 1) + number;
-        }
+    case WEST:
+        return(setShipWest(pos, size));
         break;
     default:
       return false;
@@ -254,7 +111,6 @@ function setShip(size){
 
 //Computer set ships in random locations
 function compSetBoard() {
-  // VERY Concise! I like it
   placeCarrier();
   placeBattleShip();
   placeCruiser();
@@ -262,34 +118,239 @@ function compSetBoard() {
   placeDestroyer();
 }
 
+/* Helper Methods */
+
+//Increments the column for the square ID (ex: A0, A1, B2, etc...)
+function incrementChar(column) {
+  return String.fromCharCode(column.charCodeAt(0) + 1);
+}
+
+//Set grid text based on square ID
+function insertText(id, column) {
+  document.getElementById(id).innerHTML = column;
+}
+
+function setMessage(msg) {
+	document.getElementById("message").innerText = msg;
+}
+
+//Random row generator for computer ship placements & direction
+function getRandomRow(min, max) {
+  return Math.floor( Math.random() * ( 1 + max - min ) ) + min;
+}
+
+//Random column generator for computer ship placements
+function getRandomColumn() {
+  return randomColumn = ('ABCDEFGHIJ').split('')[(Math.floor(Math.random() * 10 ))];
+}
+
+//Random location generator for computer ship placements
+function getRandomLocation() {
+  var column = getRandomColumn();
+  var row = getRandomRow(1,10);
+  var pos = column + row;
+  return pos;
+}
+
+//Random direction generator for possible computer ship placements
+function getRandomDirection(column, row) {
+  //Direction to place - 1 NORTH, 2 EAST, 3 SOUTH, 4 WEST
+  var dir = 0;
+  var lastRow = 10;
+
+  /* Checking if randomized location is on the border of the grid */
+  //If in first row, ship cannot be placed up
+  if(row === NORTH) {
+    if(column === 'A'){
+      //If also in column A, ship cannot be placed left
+      dir = getRandomRow(EAST, SOUTH);
+    }
+    else if(column === 'J'){
+      //If also in column J, ship cannot be placed right
+      dir = getRandomRow(SOUTH, WEST);
+    }
+    else{
+      dir = getRandomRow(EAST, WEST);
+    }
+  }
+  //If in column A, ship cannot be placed left
+  else if(column === 'A'){
+    //If also in last row, ship cannot be placed down
+    if(row === lastRow){
+      dir = getRandomRow(NORTH, EAST);
+    }
+    else{
+      dir = getRandomRow(NORTH, SOUTH);
+    }
+  }
+  //If in column J, ship cannot be placed right
+  else if(column === 'J'){
+    //If also in last row, ship cannot be placed down
+    if(row === lastRow){
+      dir = getRandomRow(NORTH, EAST);
+      //Since getRandomRow returns numbers 1-2, let EAST(2) represent WEST(4)
+      if(dir === EAST){
+        dir = WEST;
+      }
+    }
+    else{
+      dir = getRandomRow(NORTH, SOUTH);
+      if(dir === EAST){
+        dir = WEST;
+      }
+    }
+  }
+  //If location is not on border, ship can be in any direction
+  else{
+    dir = getRandomRow(NORTH, WEST);
+  }
+  return dir;
+}
+
+//Returns the adjacent north position from pos
+function decrementPosVertically(pos, column) {
+  return column + String.fromCharCode(pos.charCodeAt(1) - 1);
+}
+
+//Returns the adjacent west position from pos
+function decrementPosHorizontally(pos, row) {
+  return String.fromCharCode(pos.charCodeAt(0) - 1) + row;
+}
+
+//Returns the adjacent south position from pos
+function incrementPosVertically(pos, row) {
+  return String.fromCharCode(pos.charCodeAt(0) + 1) + row;
+}
+
+//Returns the adjacent east position from pos
+function incrementPosHorizontally(pos, column) {
+  return column + String.fromCharCode(pos.charCodeAt(1) + 1);
+}
+
+//Computer set ship in north orientation
+function setShipNorth(pos, size) {
+  var column = pos[0];
+  var row = pos[1];
+
+  if(row<size){
+    return false;
+  }
+  for(i=0; i<size; i++){
+    if(document.getElementById(pos).getAttribute('data') === OCCUPIED){
+      return false;
+    }
+    pos = decrementPosVertically(pos, column);
+  }
+  //Ship can be placed on map
+  pos = column + row;
+  for(j=0; j<size; j++){
+    document.getElementById(pos).setAttribute('data', OCCUPIED);
+    document.getElementById(pos).innerHTML = 'X';
+    pos = decrementPosVertically(pos, column);
+  }
+  return true;
+}
+
+//Computer set ship in east orientation
+function setShipEast(pos, size) {
+  var column = pos[0];
+  var row = pos[1];
+
+  if(column>(String.fromCharCode(FIRSTCOL.charCodeAt(0) + (size - 1)))){
+    return false;
+  }
+  for(i=0; i<size; i++){
+    if(document.getElementById(pos).getAttribute('data') === OCCUPIED){
+      return false;
+    }
+    pos = incrementPosVertically(pos, row);
+  }
+  //Ship can be placed on map
+  pos = column + row;
+  for(j=0; j<size; j++){
+    document.getElementById(pos).setAttribute('data', OCCUPIED);
+    document.getElementById(pos).innerHTML = 'X';
+    pos = incrementPosVertically(pos, row);
+  }
+  return true;
+}
+
+//Computer set ship in south orientation
+function setShipSouth(pos, size) {
+  var column = pos[0];
+  var row = pos[1];
+
+  if(row>size){
+    return false;
+  }
+  for(i=0; i<size; i++){
+    if(document.getElementById(pos).getAttribute('data')=== OCCUPIED){
+      return false;
+    }
+    pos = incrementPosHorizontally(pos, column);
+  }
+  //Ship can be placed on map
+  pos = column + row;
+  for(j=0; j<size; j++){
+    document.getElementById(pos).setAttribute('data', OCCUPIED);
+    document.getElementById(pos).innerHTML = 'X';
+    pos = incrementPosHorizontally(pos, column);
+  }
+  return true;
+}
+
+//Computer set ship in west orientation
+function setShipWest(pos, size) {
+  var column = pos[0];
+  var row = pos[1];
+
+  if(column<(String.fromCharCode(FIRSTCOL.charCodeAt(0) + (size - 1)))) {
+    return false;
+  }
+  for(i=0; i<size; i++){
+    if(document.getElementById(pos).getAttribute('data') === OCCUPIED) {
+      return false;
+    }
+    pos = decrementPosHorizontally(pos, row);
+  }
+  //Ship can be placed on map
+  pos = column + row;
+  for(j=0; j<size; j++) {
+    document.getElementById(pos).setAttribute('data', OCCUPIED);
+    document.getElementById(pos).innerHTML = 'X';
+    pos = decrementPosHorizontally(pos, row);
+  }
+  return true;
+}
+
 //5 Spaces
 function placeCarrier() {
-  var result=false;
+  var result = false;
   while(!result) {
-    result=setShip(5);
+    result = setShip(5);
   }
 }
 
 //4 Spaces
 function placeBattleShip() {
-  var result=false;
+  var result = false;
   while(!result){
-    result=setShip(4);
+    result = setShip(4);
   }
 }
 
 //3 Spaces
 function placeCruiser() {
-  var result=false;
+  var result = false;
   while(!result){
-    result=setShip(3);
+    result = setShip(3);
   }
 }
 
 //2 Spaces
 function placeDestroyer() {
-  var result=false;
+  var result = false;
   while(!result){
-    result=setShip(2);
+    result = setShip(2);
   }
 }
